a15d928860790074263dfa2bb9aa584b0bb64d18
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-09-01 11:21:27 +0430

    ADD theme skeleton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from "react-redux"
+import myAppTheme from './components/theme/myAppTheme';
 import './App.scss';
 import { addItem, changeStateItem } from "./components/redux/Actions"
-import { Container, Grid } from '@material-ui/core';
+import { Container, Grid, ThemeProvider } from '@material-ui/core';
 import HeaderDate from './components/uiComponents/headerDate';
 import Fab from '@material-ui/core/Fab';
 import TextField from '@material-ui/core/TextField'
@@ -14,6 +15,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 
 function App() {
+
   const [taskChange, setTaskChange] = useState("");
   const tasksObject = useSelector(state => state.tasks)
   const dispatch = useDispatch()
@@ -34,47 +36,49 @@ function App() {
 
   return (
     <div className="App">
-      <Container disableGutters={true} maxWidth="md" style={{ height: "100vh" }}>
-        <Grid xs={12} style={{ backgroundColor: "#455a64", height: "15vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <HeaderDate />
-        </Grid>
-        <Grid container direction={"column"} xs={12} style={{ height: "85vh", direction: "rtl" }}>
-          <Grid item container direction={"row"} wrap={"nowrap"} alignItems={"center"} style={{ height: "20vh", padding: "0px 20px" }}>
-            <Fab onClick={() => handelAdd()} color="primary" aria-label="add">
-              <AddIcon />
-            </Fab>
-            <Grid xs={12} style={{ padding: "0px 20px" }}>
-              <TextField
-                onChange={handelChange}
-                value={taskChange}
-                placeholder="برنامه های خود را برای امروز وارد کنید !"
-                autoFocus
-                fullWidth
-                variant="standard"
-              />
+      <ThemeProvider theme={myAppTheme}>
+        <Container disableGutters={true} maxWidth="md" style={{ height: "100vh" }}>
+          <Grid xs={12} style={{ backgroundColor: "#455a64", height: "15vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <HeaderDate />
+          </Grid>
+          <Grid container direction={"column"} xs={12} style={{ height: "85vh", direction: "rtl" }}>
+            <Grid item container direction={"row"} wrap={"nowrap"} alignItems={"center"} style={{ height: "20vh", padding: "0px 20px" }}>
+              <Fab onClick={() => handelAdd()} color="primary" aria-label="add">
+                <AddIcon />
+              </Fab>
+              <Grid xs={12} style={{ padding: "0px 20px" }}>
+                <TextField
+                  onChange={handelChange}
+                  value={taskChange}
+                  placeholder="برنامه های خود را برای امروز وارد کنید !"
+                  autoFocus
+                  fullWidth
+                  variant="standard"
+                />
+              </Grid>
+            </Grid>
+            <Grid item style={{ height: "65vh", padding: "0px 20px", overflowY: "scroll" }}>
+              <List>
+                {tasksObject.map((value, index) => {
+                  const labelId = `checkbox-list-label-${index}`;
+                  return (
+                    <ListItem key={index} role={undefined} button onClick={handleToggle(index)}>
+                      <ListItemIcon>
+                        <Checkbox
+                          checked={value.done}
+                          disableRipple
+                          inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                      </ListItemIcon>
+                      <ListItemText style={{ textAlign: "start" }} id={labelId} primary={value.taskName} />
+                    </ListItem>
+                  );
+                })}
+              </List>
             </Grid>
           </Grid>
-          <Grid item style={{ height: "65vh", padding: "0px 20px", overflowY: "scroll" }}>
-            <List>
-              {tasksObject.map((value, index) => {
-                const labelId = `checkbox-list-label-${index}`;
-                return (
-                  <ListItem key={index} role={undefined} button onClick={handleToggle(index)}>
-                    <ListItemIcon>
-                      <Checkbox
-                        checked={value.done}
-                        disableRipple
-                        inputProps={{ 'aria-labelledby': labelId }}
-                      />
-                    </ListItemIcon>
-                    <ListItemText style={{ textAlign: "start" }} id={labelId} primary={value.taskName} />
-                  </ListItem>
-                );
-              })}
-            </List>
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/theme/myAppTheme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const myAppTheme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#E91E63",
+        },
+        secondary: {
+            main: "#455A64",
+        },
+    },
+});
+
+export default myAppTheme;
